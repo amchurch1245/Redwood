15428a709f90841fc0197e19cb1efff5226f7427
Aubrey Church <aubrey.church@mesang.com>
2022-08-25 08:15:19 -0600

    learning redwood

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <p>This site was created to demenostrate Redwood.</p>
+    </>
+  )
+}
+
+export default AboutPage
